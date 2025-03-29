cfa91640204b99c7aa831d20f207291345425a35
Caio Belchior <caio.belchior@hotmail.com>
2025-03-29 14:48:26 -0300

    desafio 2 concluído e colocando objetos auxiliares como atributos da classe Executor

--- src/main/java/org/care/intuitive/ChallengeExecutor.java
+++ src/main/java/org/care/intuitive/ChallengeExecutor.java
@@ -5,30 +5,34 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import java.io.File;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
 
 public class ChallengeExecutor {
+
+    private final FileDownloader fileDownloader = new FileDownloader();
+    private final FileZipper fileZipper = new FileZipper();
+    private final PdfTableExtractor extractor = new PdfTableExtractor();
+
     public void runChallengeOne() {
-        FileDownloader fileDownloader = new FileDownloader();
-        PdfZipper pdfZipper = new PdfZipper();
         List<String> urlsToDownload = getUrlsToDownload();
         for (String url : urlsToDownload) {
             fileDownloader.downloadFile(url, url.substring(url.lastIndexOf("/") + 1));
         }
 
-        pdfZipper.zipFiles(
+        fileZipper.zipFiles(
                 Path.of("src", "main", "resources", "pdfs").toFile(),
                 Path.of("src", "main", "resources", "pdfs", "pdfs.zip").toFile()
         );
     }
 
     public void runChallengeTwo() {
-        PdfTableExtractor extractor = new PdfTableExtractor();
         String pdfPath = "src/main/resources/pdfs/Anexo_I_Rol_2021RN_465.2021_RN627L.2024.pdf";
         String csvPath = "src/main/resources/csvs/Anexo_I_Rol_2021RN_465.2021_RN627L.2024.csv";
-        extractor.extractTableFromPdfAndSaveCsv(pdfPath, csvPath);
+        File csvFile = extractor.extractTableFromPdfAndSaveCsv(pdfPath, csvPath);
+        fileZipper.zipFile(csvFile, "Teste_" + csvFile.getName().replace(".csv", " ") + ".zip");
     }
 
     private List<String> getUrlsToDownload() {
