79c779f278ee419f1bdb8f6afbae6d788a9403ee
Caio Belchior <caio.belchior@hotmail.com>
2025-03-29 13:51:33 -0300

    classe refatorada para zipar qualquer tipo de arquivo

---
+++ src/main/java/org/care/intuitive/FileZipper.java
@@ -0,0 +1,52 @@
+package org.care.intuitive;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+public class FileZipper {
+    public void zipFiles(File sourceDir, File zipFile) {
+        if (!sourceDir.exists() || !sourceDir.isDirectory()) {
+            System.out.println("Diretório não encontrado!");
+            return;
+        }
+
+        try (FileOutputStream fos = new FileOutputStream(zipFile);
+             ZipOutputStream zipOut = new ZipOutputStream(fos)) {
+
+            File[] files = sourceDir.listFiles();
+            if (files == null || files.length == 0) {
+                System.out.println("Nenhum arquivo encontrado para compactação.");
+                return;
+            }
+
+            for (File file : files) {
+                if (file.isFile() && !file.getAbsolutePath().equals(zipFile.getAbsolutePath())) {
+                    addFileToZip(file, zipOut);
+                }
+            }
+            System.out.println("ZIP criado em: " + zipFile.getAbsolutePath());
+
+        } catch (IOException e) {
+            System.out.println("Erro ao criar ZIP: " + e.getMessage());
+        }
+    }
+
+    private void addFileToZip(File file, ZipOutputStream zipOut) throws IOException {
+        try (FileInputStream fis = new FileInputStream(file)) {
+            ZipEntry zipEntry = new ZipEntry(file.getName());
+            zipOut.putNextEntry(zipEntry);
+
+            byte[] buffer = new byte[1024];
+            int bytesRead;
+            while ((bytesRead = fis.read(buffer)) != -1) {
+                zipOut.write(buffer, 0, bytesRead);
+            }
+
+            zipOut.closeEntry();
+            System.out.println("Adicionado ao ZIP: " + file.getName());
+        }
+    }
+}
