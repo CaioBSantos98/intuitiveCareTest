874d36a88426a92fc3e44bc2a30f808f98aba7ef
Caio Belchior <caio.belchior@hotmail.com>
2025-03-29 14:47:03 -0300

    funcao refatorada para retornar um File e substituir abreviações por valor da legenda

--- src/main/java/org/care/intuitive/PdfTableExtractor.java
+++ src/main/java/org/care/intuitive/PdfTableExtractor.java
@@ -9,15 +9,17 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.util.List;
 
 
 public class PdfTableExtractor {
-    public void extractTableFromPdfAndSaveCsv(String pdfPath, String csvPath) {
+    public File extractTableFromPdfAndSaveCsv(String pdfPath, String csvPath) {
         createDirIfNotExist(csvPath);
+        File csvFile = new File(csvPath);
 
         try (PDDocument document = Loader.loadPDF(new File(pdfPath));
-             FileWriter fileWriter = new FileWriter(csvPath);
+             FileWriter fileWriter = new FileWriter(csvFile);
              BufferedWriter writer = new BufferedWriter(fileWriter)) {
 
             SpreadsheetExtractionAlgorithm sea = new SpreadsheetExtractionAlgorithm();
@@ -50,9 +52,12 @@ public class PdfTableExtractor {
                 }
             }
 
+            replaceAbbreviations(csvFile);
+            return csvFile;
 
         } catch (IOException ex) {
             System.out.println(ex.getMessage());
+            return null;
         }
     }
 
@@ -61,6 +66,33 @@ public class PdfTableExtractor {
         File parentDir = csvFile.getParentFile();
         if (parentDir != null && !parentDir.exists()) {
             parentDir.mkdirs();
+        }
+    }
+
+    private void replaceAbbreviations(File file) {
+        if (file == null || !file.exists()) {
+            System.out.println("Arquivo não encontrado!");
+            return;
+        }
+
+        if (!file.getName().endsWith(".csv")) {
+            System.out.println("O arquivo não é um arquivo CSV válido!");
+            return;
+        }
+
+        try {
+            List<String> lines = Files.readAllLines(file.toPath());
+
+            List<String> modifiedLines = lines.stream()
+                    .map(line -> line
+                            .replace(",OD,", ",SEG ODONTOLOGICA,")
+                            .replace(",AMB,", ",SEG AMBULATORIAL,"))
+                    .toList();
+
+            Files.write(file.toPath(), modifiedLines);
+
+        } catch (IOException e) {
+            System.out.println("Erro ao modificar CSV: " + e.getMessage());
         }
     }
 }
